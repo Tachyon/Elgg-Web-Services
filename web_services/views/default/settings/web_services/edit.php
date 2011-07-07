ce5dc4f2dc09f8fcb05b8b9243b8e78c189725d3
Saket Saurabh <tachyon.saket@gmail.com>
2011-07-07 16:05:14 +0530

    Added wire in web services settings

--- web_services/views/default/settings/web_services/edit.php
+++ web_services/views/default/settings/web_services/edit.php
@@ -9,7 +9,7 @@ echo ' ';
 echo elgg_view("input/checkboxes", array(
 			'name' => 'params[enabled_webservices]',
 			'value' => unserialize(elgg_get_plugin_setting('enabled_webservices', 'web_services')),
-			'options' => array("User" => 'user', "Blog" => 'blog', "Wire" => 'wire', "Core" => 'core')
+			'options' => array("User" => 'user', "Blog" => 'blog', "Wire" => 'wire', "Core" => 'core', "Group" => 'group')
 			));
 
 echo '</div>';
