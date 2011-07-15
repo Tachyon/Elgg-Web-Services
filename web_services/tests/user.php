f9216c61982dc111eccf161dd0f1a0dd88f274bd
Saket Saurabh <tachyon.saket@gmail.com>
2011-07-15 22:39:15 +0530

    removed elgg register user in test

--- web_services/tests/user.php
+++ web_services/tests/user.php
@@ -10,11 +10,8 @@ class ElggWebServicesUserTest extends ElggCoreUnitTest {
 	public function __construct() {
 		$this->ia = elgg_set_ignore_access(TRUE);
 		parent::__construct();
-		$username = "unittest";
-		$password = "unittest";
-		$name = "I am a Test User";
-		$email = "unittest@elgg.org";
-		register_user($username, $password, $name, $email);
+		$this->user = new ElggUser();
+		$this->user->username = 'test_username_' . rand(); 
 		// all __construct() code should come after here
 	}
 
@@ -23,10 +20,10 @@ class ElggWebServicesUserTest extends ElggCoreUnitTest {
 	 */
 	public function setUp() {
 		$this->client = new ElggApiClient(elgg_get_site_url(), '2dcbe06b8318d8b3ea72523f7135ae6edfcc75c1');
-		$result = $this->client->obtainAuthToken('unittest', 'unittest');
-		if (!$result) {
-		   echo "Wrror in getting auth token!\n";
-		}
+		//$result = $this->client->obtainAuthToken('unittest', 'unittest');
+		//if (!$result) {
+		//   echo "Error in getting auth token!\n";
+		//}
 	}
 
 	/**
@@ -41,8 +38,7 @@ class ElggWebServicesUserTest extends ElggCoreUnitTest {
 	 * Called after each test object.
 	 */
 	public function __destruct() {
-		$user = get_user_by_username('unittest');
-		var_dump($user->delete());
+		$this->user->delete();
 		
 		elgg_set_ignore_access($this->ia);
 		// all __destruct() code should go above here
@@ -60,6 +56,7 @@ class ElggWebServicesUserTest extends ElggCoreUnitTest {
 	}
 	
 	public function testUpdateProfile() {
+	
 		$profile = array('description' => 'description test',
 						'briefdescription' => 'briefdescription test',
 						'location' => 'India',
@@ -71,26 +68,22 @@ class ElggWebServicesUserTest extends ElggCoreUnitTest {
 						'website' => 'http://mywebsite.com',
 						'twitter' => 'tweet',
 						);
-		$params = array('username' => 'unittest',
+		$params = array('username' => $this->user->username,
 						'profile' => $profile
 						);	
-		$user = get_user_by_username('unittest');
 		$user_fields = elgg_get_config('profile_fields');
-		
 		$results = $this->client->post('user.updateprofile', $params);
-		
 		foreach ($user_fields as $key => $type) {
-			$this->assertEqual($profile[$key], $user->$key);
+			$this->assertEqual($profile[$key], $this->user->$key);
 		}
 	}
 	
 	public function testGetProfile() {
-		$params = array('username' => 'unittest');	
+		$params = array('username' => $this->user->username);	
 		$results = $this->client->get('user.getprofile', $params);
-		$user = get_user_by_username('unittest');
 		$user_fields = elgg_get_config('profile_fields');
 		foreach ($user_fields as $key => $type) {
-			$this->assertEqual($results->$key, $user->$key);
+			$this->assertEqual($results->$key, $this->user->$key);
 		}
 	}
 }
